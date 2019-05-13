0de596a23d54e4bb649dc4838410ad1c9b2c66cc
Callriver <942129639@qq.com>
2019-05-13 16:00:04 +0800

    2

--- TimingShutdown/Program.cs
+++ TimingShutdown/Program.cs
@@ -15,6 +15,7 @@ namespace TimingShutdown
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+
             Application.Run(new Form1());
         }
     }
---
+++ TimingShutdown/Util.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TimingShutdown
+{
+    class Util
+    {
+        public static double txtConvertToInt(string txt)
+        {
+            double result = 0;
+            bool f = double.TryParse(txt, out result);
+            return result;
+        }
+    }
+}
